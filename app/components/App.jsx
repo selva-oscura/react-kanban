e9d3296f70db23703a822adf428454fce4dba605
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-11 11:49:37 -0800

    added surrounding div; added button (w/ onClick event handler to console.log 'add note')

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,4 +1,12 @@
 import React from 'react';
 import Notes from './Notes';
 
-export default () => <Notes />;
+const App = () => (
+	<div>
+		<button onClick={() =>  console.log('add note')}>+</button>
+		<Notes />
+	</div>
+)
+
+
+export default App;
