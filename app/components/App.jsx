c8b02a209297a6715dc3b4f9f39903b1f1be79fd
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-11 12:05:23 -0800

    added state to App (class ... extends, addition of constructor for props, state), population of notes from state to make it reactive

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,23 +2,31 @@ import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-];
-
-const App = () => (
-	<div>
-		<button onClick={() =>  console.log('add note')}>+</button>
-		<Notes notes={notes} />
-	</div>
-)
-
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			notes: [
+				{
+					id: uuid.v4(),
+					task: 'Learn React'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Do laundry'
+				}
+			]
+		};
+	}
+	render(){
+		const {notes} =this.state;
+		return(
+			<div>
+				<button onClick={() =>  console.log('add note')}>+</button>
+				<Notes notes={notes} />
+			</div>
+		)		
+	}
+}
 
 export default App;
