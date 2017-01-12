8ffe8f402464551e491525f8b803c06e60f03bc9
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-11 23:10:20 -0800

    added activateNoteEdit method (triggered by onNoteClick) and editNote method (triggered by onEdit) to update note in state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -33,6 +33,27 @@ class App extends React.Component {
 			notes: this.state.notes.filter(note => note.id !==id)
 		});
 	}
+	activateNoteEdit = (id) => {
+		this.setState({
+			notes: this.state.notes.map(note => {
+				if(note.id===id){
+					note.editing = true;
+				}
+				return note
+			})
+		});
+	}
+	editNote = (id, task) => {
+		this.setState({
+			notes: this.state.notes.map(note => {
+				if(note.id===id){
+					note.editing = false;
+					note.task = task;
+				}
+				return note;
+			})
+		});
+	}
 	render(){
 		const {notes} = this.state;
 		return(
@@ -40,6 +61,8 @@ class App extends React.Component {
 				<button onClick={this.addNote}>+</button>
 				<Notes 
 					notes={notes} 
+					onNoteClick={this.activateNoteEdit}
+					onEdit={this.editNote}
 					onDelete={this.deleteNote} 
 				/>
 			</div>
