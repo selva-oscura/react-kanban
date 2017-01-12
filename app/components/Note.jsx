71e855a225ff1f138ae2b90fee9b0104f47891cb
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-11 21:59:22 -0800

    added button with onClick event handler to trigger onDelete

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Note = ({task}) => <div>{task}</div>;
+const Note = ({task, onDelete}) => (
+	<div>
+		<span>{task}</span>
+		<button onClick={onDelete}>x</button>
+	</div>
+);
 
 export default Note;
