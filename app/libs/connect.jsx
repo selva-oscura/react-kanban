3e79c8b59c0fad1dd8c800537dddc8270b3ef9b9
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-12 18:04:19 -0800

    created custom connect to take state and actions and return an updated store

---
+++ app/libs/connect.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export default (state, actions) => {
+	if(typeof state==='function' ||
+		(typeof state==='object' && Object.keys(state).length)){
+		return target => connect(state, actions, target);
+	}
+	return target => props => (
+		<target {...Object.assign({}, props, actions)} />
+	);
+}
+
+// Connect to Alt through context. (Not optimised as coded here)
+// If Alt store changes, it will force render.
+// See "AltContainer" and "connect-alt" for optimised solutions
+function connect(state = () => {}, actions = {}, target){
+	class Connect extends React.Component{
+		componentDidMount(){
+			const {flux} = this.context;
+			flux.FinalStore.listen(this.handleChange);
+		}
+		componentWillUnmount(){
+			const {flux} = this.context;
+			flux.FinalStore.unlisten(this.handleChange);
+		}
+		handleChange = () => {
+			this.forceUpdate();
+		}
+		render(){
+			const {flux}  = this.context;
+			const stores = flux.stores;
+			const composedStores = composeStores(stores);
+			return React.createElement(target,
+			{
+				...Object.assign(
+					{}, this props, state(composedStores), actions
+				)
+			});
+		}
+	}
+	Connect.contextTypes = {
+		flux: React.PropTypes.object.isRequired
+	}
+	return Connect;
+}
+
+// Transform {store: <AltStore>} to {<store>: store.getState()}
+function composeStores(stores){
+	let ret = {};
+	Object.keys(stores).forEach(k => {
+		const store = stores[k];
+
+		// Combine store state
+		ret = Object.assign({}, ret, store.getState());
+	});
+	return ret;
+}
