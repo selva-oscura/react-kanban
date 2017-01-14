f17521f5cec22dc3974e11d5dd1724c65b57c032
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-14 00:02:53 -0800

    implemented wrapper for localStorage

---
+++ app/libs/storage.js
@@ -0,0 +1,13 @@
+export default storage => ({
+	get(key){
+		try{
+			return JSON.parse(storage.getItem(key));
+		}
+		catch(e){
+			return null;
+		}
+	},
+	set(key, value){
+		storage.setItem(key, JSON.stringify(value));
+	}
+});
