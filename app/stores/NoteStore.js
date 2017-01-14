8f700e48d8abb5e01c82e68bf50bfa9e9d4f7d6c
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-13 21:35:59 -0800

    created NoteStore with same default notes as had been in App state

---
+++ app/stores/NoteStore.js
@@ -0,0 +1,18 @@
+import uuid from 'uuid';
+
+class NoteStore{
+	constructor(){
+		this.notes = [
+			{
+				id: uuid.v4(),
+				task: 'Learn React'
+			},
+			{
+				id: uuid.v4(),
+				task: 'Do laundry'
+			}
+		];
+	}
+}
+
+export default NoteStore;
