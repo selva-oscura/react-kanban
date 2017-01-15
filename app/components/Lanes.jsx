a83f9f39a547645ed3837f28bd2a54098d0b88ca
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-14 21:52:21 -0800

    corrected typo

--- app/components/Lanes.jsx
+++ app/components/Lanes.jsx
@@ -4,7 +4,7 @@ import Lane from './Lane';
 const Lanes = ({lanes}) => {
 	<div className="lanes">
 		{lanes.map(lane => 
-			<Land className="lane" key={lane.id} lane={lane} />
+			<Lane className="lane" key={lane.id} lane={lane} />
 		)}
 	</div>
 }
