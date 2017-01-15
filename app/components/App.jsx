8d0fb79f135df3cb41e8d3b951ffcc32e702346a
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-14 22:04:26 -0800

    update Lane to import Lanes and LaneActions, button to add a lane, event handler to create a lane (passed to LaneActions and new data passed to LaneStore), display of Lanes Component; commented out Notes-related code

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,58 +1,84 @@
 import React from 'react';
 import uuid from 'uuid';
-import Notes from './Notes';
+// import Notes from './Notes';
 import connect from '../libs/connect';
-import NoteActions from '../actions/NoteActions';
+// import NoteActions from '../actions/NoteActions';
+import Lanes from './Lanes';
+import LaneActions from '../actions/LaneActions';
 
-class App extends React.Component {
-	addNote = () => {
-		this.props.NoteActions.create({
+const App = ({LaneActions, lanes}) => {
+	const addLane = () => {
+		LaneActions.create({
 			id: uuid.v4(),
-			task: 'New Task',
-		})
-	}
-	deleteNote = (id, e) => {
-		// Avoid bubbling to edit
-		e.stopPropagation();
-		this.props.NoteActions.delete(id);
-	}
-	activateNoteEdit = (id) => {
-		this.props.NoteActions.update({id, editing: true});
-	}
-	editNote = (id, task) => {
-		// this.props.NoteActions.update(({
-		// 	id, 
-		// 	editing: false, 
-		// 	task: task
-		// }));
-		const {NoteActions} = this.props;
-		NoteActions.update({id, task, editing: false});
-	}
-	render(){
-		const {notes} = this.props;
-		return(
-			<div>
-				<button 
-					className="add-note"
-					onClick={this.addNote}
-				>
-					+
-				</button>
-				<Notes 
-					notes={notes} 
-					onNoteClick={this.activateNoteEdit}
-					onEdit={this.editNote}
-					onDelete={this.deleteNote} 
-				/>
-			</div>
-		)		
-	}
+			name: 'New lane'
+		});
+	};
+	return(
+		<div>
+			<button 
+				className="add-lane"
+				onClick={addLane}
+			>
+				+
+			</button>
+			<Lanes lanes={lanes} />
+		</div>
+	);
+
+	// addNote = () => {
+	// 	this.props.NoteActions.create({
+	// 		id: uuid.v4(),
+	// 		task: 'New Task',
+	// 	})
+	// }
+	// deleteNote = (id, e) => {
+	// 	// Avoid bubbling to edit
+	// 	e.stopPropagation();
+	// 	this.props.NoteActions.delete(id);
+	// }
+	// activateNoteEdit = (id) => {
+	// 	this.props.NoteActions.update({id, editing: true});
+	// }
+	// editNote = (id, task) => {
+	// 	// this.props.NoteActions.update(({
+	// 	// 	id, 
+	// 	// 	editing: false, 
+	// 	// 	task: task
+	// 	// }));
+	// 	const {NoteActions} = this.props;
+	// 	NoteActions.update({id, task, editing: false});
+	// }
+	// render(){
+	// 	const {notes} = this.props;
+	// 	return(
+	// 		<div>
+	// 			<button 
+	// 				className="add-note"
+	// 				onClick={this.addNote}
+	// 			>
+	// 				+
+	// 			</button>
+	// 			<Notes 
+	// 				notes={notes} 
+	// 				onNoteClick={this.activateNoteEdit}
+	// 				onEdit={this.editNote}
+	// 				onDelete={this.deleteNote} 
+	// 			/>
+	// 		</div>
+	// 	)		
+	// }
 }
 
 // export default App;
 
-export default connect(({notes}) => ({
-	notes
+// export default connect(({notes}) => ({
+// 	notes
+// }),{
+// 	NoteActions
+// })(App);
+
+export default connect(({lanes}) => ({
+	lanes
 }),{
-	NoteActions
+	LaneActions
 })(App);
