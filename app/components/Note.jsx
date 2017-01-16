06e95bfbec517a67ef830c8f53dbfce1a6c7cf40
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-15 17:21:52 -0800

    imported DropTarget (react-dnd); added DropTarget annotation to Note to detect if a Note is being hovered over by another Note;

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {DragSource} from 'react-dnd';
+import {compose} from 'redux';
+import {DragSource, DropTarget} from 'react-dnd';
 import ItemTypes from '../constants/itemTypes';
 
 
-const Note = ({connectDragSource, children, ...props}) => {
-	return connectDragSource(	
+const Note = ({connectDragSource, connectDropTarget, children, ...props}) => {
+	return compose(connectDragSource, connectDropTarget)(
 		<div {...props}>
 			{children}
 		</div>
@@ -18,6 +19,18 @@ const noteSource = {
 	}
 };
 
-export default DragSource(ItemTypes.NOTE, noteSource, connect => ({
-	connectDragSource: connect.dragSource()
-}))(Note);
+const noteTarget = {
+	hover(targetProps, monitor){
+		const sourceProps = monitor.getItem();
+		console.log('dragging note', sourceProps, targetProps);
+	}
+};
+
+export default compose(
+	DragSource(ItemTypes.NOTE, noteSource, connect => ({
+		connectDragSource: connect.dragSource()
+	})),
+	DropTarget(ItemTypes.NOTE, noteTarget, connect => ({
+		connectDropTarget: connect.dropTarget()
+	}))
+)(Note)
