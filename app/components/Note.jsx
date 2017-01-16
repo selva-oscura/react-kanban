4c6cf907dec1ea260992b2524fc54841143d9146
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-15 19:40:59 -0800

    faded out Note in Lane when Note is being dragged within or between lanes to better highlight what is being moved

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -7,13 +7,18 @@ import ItemTypes from '../constants/itemTypes';
 const Note = ({
 	connectDragSource, 
 	connectDropTarget, 
+	isDragging,
+	isOver,
 	onMove,
 	id,
 	children,
 	...props
 }) => {
 	return compose(connectDragSource, connectDropTarget)(
-		<div {...props}>
+		<div 
+			style={{opacity: isDragging || isOver ? 0:1}} 
+			{...props}
+		>
 			{children}
 		</div>
 	);
@@ -37,10 +42,12 @@ const noteTarget = {
 };
 
 export default compose(
-	DragSource(ItemTypes.NOTE, noteSource, connect => ({
-		connectDragSource: connect.dragSource()
+	DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+		connectDragSource: connect.dragSource(),
+		isDragging: monitor.isDragging()
 	})),
-	DropTarget(ItemTypes.NOTE, noteTarget, connect => ({
-		connectDropTarget: connect.dropTarget()
+	DropTarget(ItemTypes.NOTE, noteTarget, (connect, monitor) => ({
+		connectDropTarget: connect.dropTarget(),
+		isOver: monitor.isOver()
 	}))
 )(Note)
